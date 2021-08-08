080ab391349ef3760275e3d3b9ed855b2a3a9590
三须鳗鱼 <32317549+ydxafl@users.noreply.github.com>
2021-08-08 21:31:08 +0800

    新增网络请求示例页

---
+++ lib/page/network/net_work_main.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class NetWorkMainPage extends StatelessWidget {
+  
+  const NetWorkMainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+}
